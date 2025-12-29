4f988651455eeab0923842aee1d2057c81fff8df
Ratul Sur <ratulsur@gmail.com>
2025-12-29 23:18:15 +0530

    client updated

--- client.py
+++ client.py
@@ -1,32 +1,30 @@
-from mcp.server.fastmcp import FastMCP
+from langchain_mcp_adapters import MultiServerMCPClient
+from langchain.agents import create_react_agent
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+load_dotenv()
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+import asyncio
+import os
 
-mcp = FastMCP("math")
+async def main():
+    client = MultiServerMCPClient(
+        {
+            "calculator": {
+                "command": "python",
+                "args": ["calculator.py"],
+                "transport": "stdio",
+            },
+            "weather": {
+                "url": "http://127.0.0.1:8000",
+                "transport": "streamable_http",
+            },
+        }
+    )
 
-@mcp.tool()
-def add(a:int, b:int) -> int:
-    """
-    Docstring for add
+    tools = await client.get_tools()
+
+    model = ChatGroq(model="qwen-qwq-32b")
+    agent = create_react_agent(tools, model)
+
     
-    :param a: Description
-    :type a: int
-    :param b: Description
-    :type b: int
-    :return: Description
-    :rtype: int
-    """
-    return a+b
-
-@mcp.tool()
-def multiply(a:int, b:int)-> int:
-    """
-    Docstring for multiply
-    
-    :param a: Description
-    :type a: int
-    :param b: Description
-    :type b: int
-    :return: Description
-    :rtype: int
-    """
-    return a*b
-
